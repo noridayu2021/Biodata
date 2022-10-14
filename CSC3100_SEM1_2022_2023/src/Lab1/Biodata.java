59235eb13a55af2dbd1ac103c43bde4a9fd1d100
noridayu2021 <90681115+noridayu2021@users.noreply.github.com>
2022-10-14 14:30:10 +0800

    Add files via upload

---
+++ CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
@@ -0,0 +1,13 @@
+package Lab1;
+ /* Tell me something about yourself and what you intend to learn from this course in a few words, and then show me everything on the console. */
+
+public class Biodata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hai everyone...");
+
+	}
+
+}
