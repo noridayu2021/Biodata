d27c03631265ac89ba57c42138ac928aa4720bc4
Aisyhhumaira <115607733+Aisyhhumaira@users.noreply.github.com>
2022-10-17 22:19:01 +0800

    Update Biodata.java

--- CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
+++ CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
@@ -7,7 +7,8 @@ public class Biodata {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hai everyone...");
-		
+		System.out.println(" I'm Aisyah Humaira and you guys can call me Aisyah. I'm currently live in Bangi, which is only a few minutes away from UPM");
+		System.out.println(" What I'm really looking forward in this course is to become an expert and occupy one of the best Computer Science jobs, which i need to develop certain skills")
 
 	}
 
