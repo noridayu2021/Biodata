c77cfdc9493aa5736fea7ea136ee8f9bc47acb70
syakirrr <115607610+syakirrr@users.noreply.github.com>
2022-10-17 23:14:47 +0800

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,14 @@
+package Lab1;
+ /* Tell me something about yourself and what you intend to learn from this course in a few words, and then show me everything on the console. */
+
+public class Biodata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hai everyone, my name is Muhammad Syakir Najmi and I am from Selangor.");
+		System.out.println("I am looking forward to learn about this course and next be an expert programmer one day");
+
+	}
+
+}
