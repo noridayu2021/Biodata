cba3ca596cf76c9bb00de11971aba889af5dd0e9
alfsyhm1 <115607562+alfsyhm1@users.noreply.github.com>
2022-10-17 22:39:34 +0800

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,14 @@
+package Lab1;
+/* Tell me something about yourself and what you intend to learn from this course in a few words, and then show me everything on the console. */
+
+public class Biodata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi everyone! I'm Alif Syahmi, from Selangor. I want to be proficient in as much programming language as possible.");
+		
+
+	}
+
+}
