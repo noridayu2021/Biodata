ae8e4901d1bc90ee87b36e83073a96f71af9a292
hazanisuhairy <115607585+hazanisuhairy@users.noreply.github.com>
2022-10-18 20:36:26 +0800

    Update Biodata.java

--- CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
+++ CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
@@ -6,8 +6,10 @@ public class Biodata {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hai everyone...");
-		
+		System.out.println("Hai everyone, my name is Hazani and please call me Hazani only");
+		System.out.println("I'm from Setapak, Kuala Lumpur");
+		System.out.println("I'm from Pahang Matriculation ;College and previously I took Physical Science");
+		System.out.println("During my matriculation year, I realized that I love Computer Science and decided to apply for it");
 
 	}
 
