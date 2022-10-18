fb5a486b5c05e27cc41972858196707008188f81
ixgnoy <115607618+ixgnoy@users.noreply.github.com>
2022-10-18 23:09:00 +0800

    Add files via upload

---
+++ Biodata.java
@@ -0,0 +1,22 @@
+// This is created by ixgnoy
+//To learn how to clone from github
+package Lab1;
+
+public class Biodata {
+	
+	 /* Tell me something about yourself and what you intend to learn from this course in a few words, and then show me everything on the console. */
+
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			System.out.println("Hai everyone.");
+			System.out.println("Wong Yong Xi here!");
+			System.out.println("I want to learn more about AI, Cybersecurity and networking.");
+
+		}
+
+	}
+
+
