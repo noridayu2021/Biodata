dbcd2b0072eb536e943e8f147877213fc7576301
intanfarzanah <115607614+intanfarzanah@users.noreply.github.com>
2022-10-19 00:34:10 +0800

    Add files via upload

---
+++ biodata.java
@@ -0,0 +1,14 @@
+
+public class biodata {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello everyone, My name is Nur Intan Farzanah and you can call me Intan");
+		System.out.println("I am from Kelantan");
+		System.out.println("I have been interested in computer system since my secondary school");
+		System.out.println("Hence, I am looking forward to learn about it in this course");
+	}
+	
+	
+
+}
