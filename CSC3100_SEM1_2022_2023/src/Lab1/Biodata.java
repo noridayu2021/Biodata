e3008e8476f788e360a0ffb7220d2b1436217567
AlhaniMawaddahBalqis <115607615+AlhaniMawaddahBalqis@users.noreply.github.com>
2022-10-18 20:13:28 +0800

    Update Biodata.java

--- CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
+++ CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
@@ -1,14 +1,16 @@
 package Lab1;
- /* Tell me something about yourself and what you intend to learn from this course in a few words, and then show me everything on the console. */
 
 public class Biodata {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main (String[]args) 
+   {
 		
-		System.out.println("Hai everyone...");
+	System.out.println("Hello everyone!");
 		
-
-	}
+        System.out.println("my full name is Alhani Mawaddah Balqis Binti Majdi,you may call me Hani.");
+		
+        System.out.println("I live in Bandar Tun Hussein Onn,Cheras.");
+		
+        System.out.println("The reason I learn Computer Science is to learn the basics of it so that I can proceed to be majoring in Cyber Security.");
+   }
 
 }
