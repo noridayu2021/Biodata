862cf094541717804c11cd0ea2cbe18d4b88a08b
hazanisuhairy <115607585+hazanisuhairy@users.noreply.github.com>
2022-10-18 20:37:57 +0800

    Update Biodata.java

--- CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
+++ CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
@@ -8,7 +8,7 @@ public class Biodata {
 		
 		System.out.println("Hai everyone, my name is Hazani and please call me Hazani only");
 		System.out.println("I'm from Setapak, Kuala Lumpur");
-		System.out.println("I'm from Pahang Matriculation ;College and previously I took Physical Science");
+		System.out.println("I'm from Pahang Matriculation College and previously I took Physical Science");
 		System.out.println("During my matriculation year, I realized that I love Computer Science and decided to apply for it");
 
 	}
