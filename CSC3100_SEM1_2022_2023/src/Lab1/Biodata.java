1b676d4f7563a8c3b58c38caa9002d549796a4c9
User <User@10.112.26.215>
2022-10-19 00:54:53 +0800

    first commit

--- CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
+++ CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
@@ -6,8 +6,10 @@ public class Biodata {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hai everyone...");
-		
+		System.out.println("Hello everyone, My name is Nur Intan Farzanah and you can call me Intan");
+		System.out.println("I am from Kelantan");
+		System.out.println("I have been interested in computer system since my secondary school");
+		System.out.println("Hence, I am looking forward to learn about it in this course");
 
 	}
 
