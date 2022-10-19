55215f08fd9452368abc1d3fe523da76adc68692
Saheerah0910 <115607780+Saheerah0910@users.noreply.github.com>
2022-10-19 09:57:02 +0800

    Update Biodata.java

--- CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
+++ CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
@@ -6,7 +6,7 @@ public class Biodata {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hai everyone...");
+		System.out.println("Hai everyone.My name is Nur Saheerah binti Rafiq Ravindran.Previously,I studied in Kolej Matrikulasi Negeri Sembilan. I took this course because I want to earn a lot of money :)");
 		
 
 	}
