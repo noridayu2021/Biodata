5f6d1e48d7dada0cd0ca74c4cd031b7b23ede9a1
Nur Aishah <Nur Aishah@LAPTOP-HN6NFCAM>
2022-10-20 09:58:17 +0800

    Biodata (Nur Aishah)

--- CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
+++ CSC3100_SEM1_2022_2023/src/Lab1/Biodata.java
@@ -6,8 +6,9 @@ public class Biodata {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hai everyone...");
-		
+		System.out.println("Hi everyone! My name is Nur Aishah.");
+		System.out.println("I am from Johor and used to be an ASPer student.");
+		System.out.println("I am looking forward on learning programming and improving my skills during my study here in UPM.");
 
 	}
 
